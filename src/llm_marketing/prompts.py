68fa3467a6d287ea89c8a5cea73c0a55bb161b67
GiuliaSerretti <serrettigiulia@gmail.com>
2025-07-18 13:23:10 +0200

    Corretto "In Store displays"

--- src/llm_marketing/prompts.py
+++ src/llm_marketing/prompts.py
@@ -180,7 +180,7 @@ Options:
 1. Social Media
 2. Emails
 3. Deal sites (fave, iprice, etc)
-4. In Store display
+4. In Store displays
 5. Billboards
 6. Through friends and word of mouth
 7. Application offer
